4d8da41279ec52b01f2cfada555ab72f9249d383
benbop <ben.erlich3@gmail.com>
2023-12-03 20:42:28 +0200

    Add project files.

---
+++ SpotifyPlaylist-alpha/Program.cs
@@ -0,0 +1,33 @@
+﻿using SpotifyAPI.Web;
+
+namespace SpotifyPlaylist_alpha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> playlistSongs = GetPLayListAsync().Result;
+            
+        }
+         public static async Task<List<string>> GetPLayListAsync()
+        {
+            List<string> PlayListSongs = new List<string>();
+            SpotifyApiCalls SpotifyApi = new SpotifyApiCalls();
+
+
+            var playlist = await SpotifyApiCalls.GetPlaylistAsync("6lyWdNcyXZmZkI7W46Dd7M");
+            foreach (PlaylistTrack<IPlayableItem> item in playlist.Tracks.Items)
+            {
+                if (item.Track is FullTrack track)
+                {
+                    var Artists = track.Artists;
+                    var artistNames = string.Join(", ", Artists.Select(a => a.Name));
+                    PlayListSongs.Add($"{track.Name} By {artistNames}");
+                    
+                }
+            }
+            return PlayListSongs;
+        }
+
+    }
+}
---
+++ SpotifyPlaylist-alpha/SpotifyApiCalls.cs
@@ -0,0 +1,36 @@
+﻿using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using SpotifyAPI.Web.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SpotifyPlaylist_alpha
+{
+    
+    internal class SpotifyApiCalls
+    {
+       
+        
+        public static async Task<FullPlaylist> GetPlaylistAsync(string playlistId)
+        {
+           
+            FullPlaylist playlist = await getAccsesTokenAsync().Result.Playlists.Get(playlistId);
+            return playlist;
+        }
+        public static async Task<SpotifyClient> getAccsesTokenAsync()
+        {
+            var config = SpotifyClientConfig.CreateDefault();
+
+            var request = new ClientCredentialsRequest("503a274f9a5c492394417f1b73716f5a", "6707f44d2ece492596137c920f2e24ec");
+            var response = await new OAuthClient(config).RequestToken(request);
+
+            var spotify = new SpotifyClient(config.WithToken(response.AccessToken));
+            return spotify;
+        }
+
+    }
+}
