81aa494d0540733f9e800ec0d34e89733027055f
benbop <105657554+benbop@users.noreply.github.com>
2023-12-11 12:57:49 +0200

    Update Program.cs

--- SpotifyPlaylist-alpha/Program.cs
+++ SpotifyPlaylist-alpha/Program.cs
@@ -9,39 +9,6 @@ namespace SpotifyPlaylist_alpha
             YoutubeApiCalls Youtube = new YoutubeApiCalls();
             List<string> spotifyPlaylist = await SpotifyApiCalls.GetPLayListAsync();
             await YoutubeApiCalls.SetPlaylist(spotifyPlaylist);
-            
-            //try
-            //{
-            // Step 1: Get Spotify Playlist
-            //List<string> spotifyPlaylist = await SpotifyApiCalls.GetPLayListAsync();
-
-            //if (spotifyPlaylist.Count == 0)
-            //{
-            //    Console.WriteLine("No songs found in the Spotify playlist. Exiting.");
-            //    return;
-            //}
-
-            // Step 2: Convert Spotify Playlist to YouTube Playlist
-            // Assuming the first song in the Spotify playlist as the keyword for YouTube search
-            //    string youtubeKeyword = spotifyPlaylist[0];
-            //    var youtubeSearchRequest = YoutubeApiCalls.Searcher(youtubeKeyword);
-            //    var youtubeSearchResponse = youtubeSearchRequest.Execute();
-
-            //    YoutubeApiCalls.PlayListMaker();  // Creates a new playlist on YouTube
-
-            //    // Convert Spotify songs to YouTube videos and add them to the playlist
-            //    YoutubeApiCalls.VideoIds(spotifyPlaylist);
-
-            //    Console.WriteLine("Conversion completed successfully!");
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine($"An error occurred: {ex.Message}");
-            //}
-            //}
-
-
-
         }
     }
 }
